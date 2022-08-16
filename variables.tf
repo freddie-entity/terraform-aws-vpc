93013ad23eca524925af2ce1e6a36c55596c393d
[freddie-entity] <nguyentrungtinc4nh1516@gmail.com>
2022-08-16 11:32:53 +0700

    Var options

--- variables.tf
+++ variables.tf
@@ -2,4 +2,9 @@ variable "vpc_cidr" {
   description = "VPC CIDR"
   default = "10.0.0.0/16"
   type = string
+  validation {
+    
+    condition     = contains(["10.0.0.0/16", "10.0.1.0/16"], var.vpc_cidr)
+    error_message = "The vpc_cidr '10.0.0.0/16' or '10.0.1.0/16'"
+  }
 }
